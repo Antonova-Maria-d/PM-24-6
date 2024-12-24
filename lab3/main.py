ac48bd71620adc9f4d6abac83bccf5834d94bbf4
Антонова Мария <antonova.mashenka@gmail.com>
2024-12-24 22:46:50 +0300

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -1,46 +1,41 @@
 import pathlib
+from pathlib import Path
 import csv
-def load_table(file):
-    table = []
-    test = pathlib.Path(file)
-    if not test.is_file():
-        raise FileNotFoundError("Файл не найден.")
-     with open(file, 'r', newline = '', encoding = 'utf-8') as csvfile: 
-    i = csv.reader(csvfile)
-    for row in i:
-        table.append(row)
-    return table
-
-def save_table(table, file):
-    test = pathlib.Path(file)
-    if not test.is_file():
-        raise FileNotFoundError("Файл не найден.")
-    with open(file, 'w', newline = '', encoding = 'utf-8') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(table)
-
 import pickle
-
 def load_table(file):
     test = pathlib.Path(file)
     if not test.is_file():
         raise FileNotFoundError("Файл не найден.")
-    with open(file, 'rb', encoding = 'utf-8') as picfile:
+    file_path = Path(file)
+    if file_path.suffix.lower() == '.csv':
+        with open(file, 'r', newline = '', encoding = 'utf-8') as csvfile: 
+            table = []
+            i = csv.reader(csvfile)
+            for row in i:
+                table.append(row)
+        return table
+    elif file_path.suffix.lower() == '.pkl':
+        with open(file, 'rb', encoding = 'utf-8') as picfile:
         table = pickle.load(picfile)
-    return table
-
+        return table
+    else:
+        raise ValueError("Непредвиденное расширение файла.")
+    
 def save_table(table, file):
     test = pathlib.Path(file)
     if not test.is_file():
         raise FileNotFoundError("Файл не найден.")
-    with open(file, 'wb', encoding = 'utf-8') as picfile:
-        pickle.dump(table, picfile) 
-
-
-def save_table(table, file):
-    test = pathlib.Path(file)
-    if not test.is_file():
-        raise FileNotFoundError("Файл не найден.")
-    with open(file, 'w', encoding = 'utf-8') as txtfile:
-        for row in table:
-            txtfile.write('\t'.join(row) + '\n')
+    file_path = Path(file)
+    if file_path.suffix.lower() == '.csv':
+        with open(file, 'w', newline = '', encoding = 'utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(table)
+    elif file_path.suffix.lower() == '.pkl':
+        with open(file, 'wb', encoding = 'utf-8') as picfile:
+            pickle.dump(table, picfile) 
+    elif if file_path.suffix.lower() == '.txt':
+        with open(file, 'w', encoding = 'utf-8') as txtfile:
+            for row in table:
+                txtfile.write('\t'.join(row) + '\n')
+    else:
+        raise ValueError("Непредвиденное расширение файла.")
